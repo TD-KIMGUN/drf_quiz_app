c73439d06917496945635d6bd9ac1ddf6cf663c8
김준성 <egoofkorea@gmail.net>
2021-01-18 18:51:27 +0900

    Ready for deploy

---
+++ quiz/serializers.py
@@ -0,0 +1,10 @@
+
+from rest_framework import serializers
+
+from quiz.models import Quiz
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ('title','body','answer')
